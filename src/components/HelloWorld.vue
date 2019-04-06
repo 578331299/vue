c2a374241d80ad237716153d9e83379f07086274
“sherry q wang <578331299@qq.com>
2019-04-06 13:38:14 +0800

    first  session
    first  session

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1 :title="'hello' + titleMsg">{{ msg }}</h1>
+    <div>
+      <span v-html="number"></span>
+      <button  v-text="text" @click="handleClick"></button>
+    </div>
+    <input type="text" v-model="inputValue">
+    <p>{{inputValue}}</p>
   </div>
 </template>
 
 <script>
+// 面向数据编程
+// 事件绑定 v-click--@click
+// 属性绑定 v-bind:---:
+// 双向数据绑定 v-model
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to my first application',
+      titleMsg: 'Welcome sherry',
+      number: '<span>123</span>',
+      text: 'test button',
+      inputValue: ''
+    }
+  },
+  methods: {
+    handleClick: function () {
+      this.number = '456'
     }
   }
 }
@@ -110,4 +50,5 @@ li {
 a {
   color: #42b983;
 }
+
 </style>
